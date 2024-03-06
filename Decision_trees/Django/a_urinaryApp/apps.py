cd20cda651056c0ab3fe8b72f78991374bf28d0b
Timileyin Samuel Akintilo <akintilotimileyin@gmail.com>
2024-03-06 21:06:20 +0000

    Updating portfolio files

--- Decision_trees/Django/a_urinaryApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AUrinaryappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'a_urinaryApp'
--- Decision_trees/Django/a_urinaryApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.predictor, name='predictor'),
-    path('dtm/', views.predictor, name='predictor'),
-    path('result', views.formInfo, name='result'),
-    path('dtm/result', views.formInfo, name='result'),
-]
